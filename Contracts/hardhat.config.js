191648e17174d88f86c5c9c8475c0fb32f8378a1
Ashish <ashishgugale73@gmail.com>
2023-12-21 16:46:50 +0530

    New branch

--- Contracts/hardhat.config.js
+++ Contracts/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+}
